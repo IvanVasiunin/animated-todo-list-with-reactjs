2798660e9a8bbf0994f527b4fe6e9343efd263e7
Ivan <ivanvasyunin@gmail.com>
2024-02-19 18:03:59 +0100

    initial files added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,15 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import WelcomePage from '../pages/Welcome.jsx';
+import ChallengesPage from '../pages/Challenges.jsx';
+
+const router = createBrowserRouter([
+  { path: '/', element: <WelcomePage /> },
+  { path: '/challenges', element: <ChallengesPage /> },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
